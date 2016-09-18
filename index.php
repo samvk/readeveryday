689b97bc8292f0c116a6fce3c09cfdd0c9da2c13
samvnkauffman-pc <samvnkauffman@gmail.com>
2016-09-17 22:26:40 -0400

    rename form-disabled class

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     
 	<!-- Custom CSS -->
-	<link href="css/style.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 
 </head>
 
